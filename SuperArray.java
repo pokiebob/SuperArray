c4661bbfbbdec57b48451a55b4c180c4900a3d90
pokiebob <ccursetjee@gmail.com>
2020-11-02 20:07:52 -0500

    set

--- SuperArray.java
+++ SuperArray.java
@@ -22,12 +22,19 @@ public class SuperArray {
     return data[index];
   }
 
+  public String set(int index, String element) {
+    data[index] = element;
+    return element;
+  }
+
   public static void main(String[] args) {
     SuperArray words = new SuperArray();
     System.out.println(words.size());
-    if (words.add("Hello")) {
+    if (words.add("Hi")) {
       System.out.println(words.size());
       System.out.println(words.get(0));
     }
+    System.out.println(words.set(0, "Hello"));
+    System.out.println(words.get(0));
   }
 }
