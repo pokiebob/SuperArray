d004ed65dea7defbe05f4fac0a480e0276a12363
pokiebob <ccursetjee@gmail.com>
2020-11-02 20:01:22 -0500

    add

--- SuperArray.java
+++ SuperArray.java
@@ -5,10 +5,24 @@ public class SuperArray {
 
   public SuperArray() {
     size = 0;
-    data = new String[0];
+    data = new String[10];
+  }
+
+  public int size() {
+    return size;
+  }
+
+  public boolean add(String element) {
+    data[size] = element;
+    size++;
+    return true;
   }
 
   public static void main(String[] args) {
-
+    SuperArray words = new SuperArray();
+    System.out.println(words.size());
+    if (words.add("Hello")) {
+      System.out.println(words.size());
+    }
   }
 }
