66cde138da9f327ae70a0b45cabcd69ca98b642a
pokiebob <ccursetjee@gmail.com>
2020-11-02 19:28:29 -0500

    constructor

---
+++ SuperArray.java
@@ -0,0 +1,14 @@
+public class SuperArray {
+
+  private String[] data;
+  private int size;
+
+  public SuperArray() {
+    size = 0;
+    data = new String[0];
+  }
+
+  public static void main(String[] args) {
+
+  }
+}
